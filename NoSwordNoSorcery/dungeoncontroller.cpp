fc90571eeca391bec9abbf0358ad3ed60804f22c
Jonas ten Cate <tencatejonas@gmail.com>
2022-01-28 18:27:02 +0100

    Render des portes si 2D ou 3D

--- NoSwordNoSorcery/dungeoncontroller.cpp
+++ NoSwordNoSorcery/dungeoncontroller.cpp
@@ -3,7 +3,8 @@
 
 
 DungeonController::DungeonController(){
-    this->View = new DungeonView2D();
+    this->View = new DungeonView3D();
+    this->Viewis3D = true;
     this->Model = new DungeonModel();
 }
 
@@ -12,10 +13,12 @@ void DungeonController::OnViewSwitched(){
     if(this->Viewis3D){
         delete this->View;
         this->View = new DungeonView2D();
+        this->Viewis3D = false;
     }
     else{
         delete this->View;
         this->View = new DungeonView3D();
+        this->Viewis3D = true;
     }
 }
 
@@ -23,14 +26,24 @@ void DungeonController::Render(void){
     if(((DungeonModel *)this->Model)->TopDoor()){
         ((ADungeonView *)this->View)->DrawTopDoor();
     }
-    if(((DungeonModel *)this->Model)->BottomDoor()){
-        ((ADungeonView *)this->View)->DrawBottomDoor();
-    }
     if(((DungeonModel *)this->Model)->LeftDoor()){
         ((ADungeonView *)this->View)->DrawLeftDoor();
     }
-    if(((DungeonModel *)this->Model)->RightDoor()){
-        ((ADungeonView *)this->View)->DrawRightDoor();
+    if(!Viewis3D){
+        if(((DungeonModel *)this->Model)->RightDoor()){
+            ((ADungeonView *)this->View)->DrawRightDoor();
+        }
+        if(((DungeonModel *)this->Model)->BottomDoor()){
+            ((ADungeonView *)this->View)->DrawBottomDoor();
+        }
     }
     AController::Render();
+    if(Viewis3D){
+        if(((DungeonModel *)this->Model)->RightDoor()){
+            ((ADungeonView *)this->View)->DrawRightDoor();
+        }
+        if(((DungeonModel *)this->Model)->BottomDoor()){
+            ((ADungeonView *)this->View)->DrawBottomDoor();
+        }
+    }
 }
